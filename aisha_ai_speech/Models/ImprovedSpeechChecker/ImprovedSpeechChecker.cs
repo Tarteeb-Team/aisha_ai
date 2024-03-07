c58022582ec62c606efa3ebfcfaac59cb0810fee
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-03-07 23:06:40 +0900

    DATA: Create Models

---
+++ aisha_ai_speech/Models/ImprovedSpeechChecker/ImprovedSpeechChecker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace aisha_ai_speech.Models.ImprovedSpeechChecker
+{
+    public class ImprovedSpeechChecker
+    {
+        public Guid Id { get; set; }
+        public bool State { get; set; }
+        public string TelegramUserName { get; set; }
+    }
+}
